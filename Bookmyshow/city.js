fd8a02b289dc3c66bc6f4142a42bd227471aee82
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-07-23 18:43:51 +0530

    Create city.js

---
+++ Bookmyshow/city.js
@@ -0,0 +1,16 @@
+function Myfunction() { 
+    var show = document.getElementById("MyShow");
+    if(show.style.display == "none"){
+        show.style.display = "block";
+    }else{
+        show.style.display="none";
+    }
+}
+function viewcities() {
+    var city = document.getElementById("city");
+    if (city.style.display == "none") {
+        city.style.display = "block";
+    } else {
+        city.style.display = "none";
+    }
+}
