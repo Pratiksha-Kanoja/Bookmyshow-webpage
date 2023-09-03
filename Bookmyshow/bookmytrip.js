500ff68155ddfd66094245a6db5ac59ae008972d
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-03 13:48:52 +0530

    Create bookmytrip.js

---
+++ Bookmyshow/bookmytrip.js
@@ -0,0 +1,12 @@
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("myForm1").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("myForm1").style.display = "none";
+  }
+  
+  
