61307df721fa1dacf0e4634b7a1bfcbfccf75ced
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2023-09-04 22:23:22 +0530

    Update bookmytrip.js

--- Bookmyshow/bookmytrip.js
+++ Bookmyshow/bookmytrip.js
@@ -1,12 +1,20 @@
 
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-    document.getElementById("myForm1").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-    document.getElementById("myForm1").style.display = "none";
-  }
-  
-  
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("myForm1").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("myForm1").style.display = "none";
+}
+
+function bookticket() {
+  document.getElementById("bookticket").style.display = "block";
+  document.getElementById("ticketbook-bckgrd").style.display = "block";
+}
+
+function ticket_close() {
+  document.getElementById("bookticket").style.display = "none";
+  document.getElementById("ticketbook-bckgrd").style.display = "none";
+}
+
